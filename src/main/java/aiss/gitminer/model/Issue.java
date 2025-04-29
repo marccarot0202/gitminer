3e46e494572f2da4fa43ecfbebaa66235f8a519a
ALFONSO EDUARDO MARQUEZ CHAMORRO <amarquez6@us.es>
2025-04-29 21:28:18 +0200

    Update Issue.java

--- src/main/java/aiss/gitminer/model/Issue.java
+++ src/main/java/aiss/gitminer/model/Issue.java
@@ -17,9 +17,6 @@ public class Issue {
     @Id
     @JsonProperty("id")
     private String id;
-
-    @JsonProperty("ref_id")
-    private String refId;
     @JsonProperty("title")
     private String title;
     @JsonProperty("description")
@@ -46,14 +43,8 @@ public class Issue {
     @JoinColumn(name = "assignee_id",referencedColumnName = "id")
     @OneToOne(cascade=CascadeType.ALL)
     private User assignee;
-    @JsonProperty("upvotes")
-    private Integer upvotes;
-    @JsonProperty("downvotes")
-    private Integer downvotes;
-
-    @JsonProperty("web_url")
-    private String webUrl;
-
+    @JsonProperty("votes")
+    private Integer votes;
     @JsonProperty("comments")
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "issueId")
@@ -65,14 +56,6 @@ public class Issue {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getRefId() {
-        return refId;
-    }
-
-    public void setRefId(String refId) {
-        this.refId = refId;
     }
 
     public String getTitle() {
@@ -147,28 +130,12 @@ public class Issue {
         this.assignee = assignee;
     }
 
-    public Integer getUpvotes() {
-        return upvotes;
+    public Integer getVotes() {
+        return votes;
     }
 
-    public void setUpvotes(Integer upvotes) {
-        this.upvotes = upvotes;
-    }
-
-    public Integer getDownvotes() {
-        return downvotes;
-    }
-
-    public void setDownvotes(Integer downvotes) {
-        this.downvotes = downvotes;
-    }
-
-    public String getWebUrl() {
-        return webUrl;
-    }
-
-    public void setWebUrl(String webUrl) {
-        this.webUrl = webUrl;
+    public void setVotes(Integer votes) {
+        this.votes = votes;
     }
 
     public List<Comment> getComments() {
@@ -186,10 +153,6 @@ public class Issue {
         sb.append("id");
         sb.append('=');
         sb.append(((this.id == null) ? "<null>" : this.id));
-        sb.append(',');
-        sb.append("refId");
-        sb.append('=');
-        sb.append(((this.refId == null) ? "<null>" : this.refId));
         sb.append(',');
         sb.append("title");
         sb.append('=');
@@ -227,13 +190,9 @@ public class Issue {
         sb.append('=');
         sb.append(((this.assignee == null) ? "<null>" : this.assignee));
         sb.append(',');
-        sb.append("upvotes");
+        sb.append("votes");
         sb.append('=');
-        sb.append(((this.upvotes == null) ? "<null>" : this.upvotes));
-        sb.append(',');
-        sb.append("downvotes");
-        sb.append('=');
-        sb.append(((this.downvotes == null) ? "<null>" : this.downvotes));
+        sb.append(((this.votes == null) ? "<null>" : this.votes));
         sb.append(',');
         sb.append("comments");
         sb.append('=');
