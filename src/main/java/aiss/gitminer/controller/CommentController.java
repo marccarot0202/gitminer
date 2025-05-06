e80ba100c6f7a8a3bf6ab3908c2ea16593b8117c
aitanavp <aitvalpra@alum.us.es>
2025-05-06 17:28:52 +0200

    Comment controller

--- src/main/java/aiss/gitminer/controller/CommentController.java
+++ src/main/java/aiss/gitminer/controller/CommentController.java
@@ -1,6 +1,10 @@
 package aiss.gitminer.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import aiss.gitminer.exceptions.CommentNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +19,26 @@ import aiss.gitminer.repository.CommentRepository;
 @RequestMapping("/gitminer/comments")
 public class CommentController {
 
-    private final CommentRepository commentRepository;
+    @Autowired
+    CommentRepository commentRepository;
 
     public CommentController(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
     }
 
+    // Get all comments
     @GetMapping
     public List<Comment> findAll() {
         return commentRepository.findAll();
     }
 
+    //Get comments by id
     @GetMapping("/{id}")
-    public Comment findById(@PathVariable String id) {
-        return commentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public Comment findById(@PathVariable(value="id") String id) throws CommentNotFoundException {
+        Optional<Comment> comment = commentRepository.findById(id);
+        if (!comment.isPresent()) {
+            throw new CommentNotFoundException();
+        }
+        return comment.get();
     }
 }
