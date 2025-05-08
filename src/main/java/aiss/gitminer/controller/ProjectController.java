f05e6eb017619fe2af3eb289b3c79aea36b2f40b
aitanavp <aitvalpra@alum.us.es>
2025-05-08 11:04:07 +0200

    Extension: added search params on project controller--> pagination, size, sorting, direction and filters

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -4,6 +4,10 @@ import aiss.gitminer.exceptions.ProjectNotFoundException;
 import aiss.gitminer.model.Project;
 import aiss.gitminer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +28,27 @@ public class ProjectController {
         this.projectRepository = projectRepository;
     }
 
+    // Find projects with params: page, size, sortBy, direction and filter
     @GetMapping
-    public List<Project> findAll() {
-        return projectRepository.findAll();
+    public ResponseEntity<Page<Project>> getProjects(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction,
+            @RequestParam(required = false) String filter) {
+
+        Sort sort = Sort.by(Sort.Order.asc(sortBy));
+        if (direction.equalsIgnoreCase("desc")) {
+            sort = Sort.by(Sort.Order.desc(sortBy));
+        }
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<Project> projects;
+        if (filter != null && !filter.isEmpty()) {
+            projects = projectRepository.findByNameContaining(filter, pageable);
+        } else {
+            projects = projectRepository.findAll(pageable);
+        }
+        return ResponseEntity.ok(projects);
     }
 
     @GetMapping("/{id}")
